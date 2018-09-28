aaf2b6a9aeec26e54fce70ae0cf8cecbd256d541
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-09-28 15:26:39 -0400

    cbioportal entrypoint

---
+++ cbioportal/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+PYTHON="/usr/bin/env python3"
+MIGRATE_SCRIPT=${PORTAL_HOME}/core/src/main/scripts/migrate_db.py
+PROPERTIES_FILE=${PORTAL_HOME}/src/main/resources/portal.properties
+MIGRATION_SQL=${PORTAL_HOME}/db-scripts/src/main/resources/migration.sql
+MIGRATION_COMMAND="${PYTHON} ${MIGRATE_SCRIPT} \
+    --properties-file ${PROPERTIES_FILE} \
+    --sql ${MIGRATION_SQL}"
+RUN_CMD="/usr/local/tomcat/bin/catalina.sh run"
+
+if [ -z "${DO_DB_MIGRATE}" ]; then
+    yes y | eval ${MIGRATION_COMMAND};
+fi
+
+eval ${RUN_CMD}
