1d0e7bf609b5dd9024f3a0ea9dbb86d757545a92
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-10-01 10:33:08 -0400

    who cares about prompts

--- scripts/generate_self_signed_keys_for_nginx.sh
+++ scripts/generate_self_signed_keys_for_nginx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 MAIN_PATH=$(dirname $(dirname $(readlink -f "$0")))
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+yes | openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
  -keyout ${MAIN_PATH}/services/nginx-wrapper/cert.key \
  -out ${MAIN_PATH}/services/nginx-wrapper/cert.crt
