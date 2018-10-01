cda21709c1175c1432055897f69c7fda7e2f5bf3
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-10-01 10:50:05 -0400

    fix test self-signed-cert script

--- scripts/generate_self_signed_keys_for_nginx.sh
+++ scripts/generate_self_signed_keys_for_nginx.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
-MAIN_PATH=$(dirname $(dirname $(readlink -f "$0")))
-yes | openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
- -keyout ${MAIN_PATH}/services/nginx-wrapper/cert.key \
- -out ${MAIN_PATH}/services/nginx-wrapper/cert.crt
+
+# readlink -f shim in case not using GNU readlink
+readlink_f () {
+    TARGET_FILE=$1
+    cd $(dirname ${TARGET_FILE})
+    TARGET_FILE=$(basename ${TARGET_FILE})
+    while [ -L "${TARGET_FILE}" ]
+    do
+        TARGET_FILE=$(readlink ${TARGET_FILE})
+        cd $(dirname ${TARGET_FILE})
+        TARGET_FILE=$(basename ${TARGET_FILE})
+    done
+    PHYS_DIR=`pwd -P`
+    RESULT=${PHYS_DIR}/${TARGET_FILE}
+    echo ${RESULT}
+}
+
+MAIN_PATH=$(dirname $(dirname $(readlink_f "$0")))
+
+openssl req -new -x509 -nodes -days 365 -newkey rsa:2048 -subj "/" \
+    -keyout ${MAIN_PATH}/services/nginx-wrapper/cert.key \
+    -out ${MAIN_PATH}/services/nginx-wrapper/cert.crt
