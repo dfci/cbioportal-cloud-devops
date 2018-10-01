2f5bcc04d5499b05dd1fb0bc2417e6997a1314fe
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-10-01 14:14:17 -0400

    fix gce script

--- scripts/gce_debian_9_setup.sh
+++ scripts/gce_debian_9_setup.sh
@@ -2,7 +2,9 @@
 
 # Install Docker
 apt-get update
-apt-get install -y software-properties-common
+apt-get install apt-transport-https \
+     ca-certificates curl \
+     gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
