8067b6d1af94ff5f398d283407ca85493d92b20f
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-10-01 13:08:23 -0400

    GCE Debian helper script

---
+++ scripts/gce_debian_9_setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install Docker
+apt-get update
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+
+# Install Docker Compose
+curl -L \
+    "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Create EXT4 Filesystem on /dev/sdb
+mkfs.ext4 /dev/sdb
+
+# Add /dev/sdb to fstab
+echo "/dev/sdb  /root/cbioportal-cloud-devops/mountpoints/cbioportal-mysql-data ext4 defaults 1 1" >> /etc/fstab
+# Mount /dev/sdb
+mount /dev/sdb
