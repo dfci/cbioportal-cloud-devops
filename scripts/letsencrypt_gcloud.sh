081757a6651c677529c0129f2d8e389a36c48ea7
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-10-02 13:18:55 -0400

    letsencrypt helper script

---
+++ scripts/letsencrypt_gcloud.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# readlink -f shim in case not using GNU readlink
+readlink_f () {
+    TARGET_FILE=$1
+    cd $(dirname ${TARGET_FILE})
+    TARGET_FILE=$(basename ${TARGET_FILE})
+    while [ -L "${TARGET_FILE}" ]
+    do
+        TARGET_FILE=$(readlink ${TARGET_FILE})
+        cd $(dirname ${TARGET_FILE})
+        TARGET_FILE=$(basename ${TARGET_FILE})
+    done
+    PHYS_DIR=`pwd -P`
+    RESULT=${PHYS_DIR}/${TARGET_FILE}
+    echo ${RESULT}
+}
+
+MAIN_PATH=$(dirname $(dirname $(readlink_f "$0")))
+
+gcloud auth activate-service-account --key-file "$1"
+
+curl https://get.acme.sh | sh
+source ${HOME}/.bashrc
+echo "Please enter the domain name of this server, e.g. example.com"
+read DOMAIN
+acme.sh --test --issue -d ${DOMAIN} -d *.cbiodfci.org --dns dns_gcloud
