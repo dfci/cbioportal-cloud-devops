47b970ddd4a1c466d391c71dea685fe6fb817eb6
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2018-10-02 13:38:00 -0400

    letsencrypt helper script

--- scripts/letsencrypt_gcloud.sh
+++ scripts/letsencrypt_gcloud.sh
@@ -23,4 +23,24 @@ curl https://get.acme.sh | sh
 echo "Please enter the domain name of this server, e.g. example.com"
 read DOMAIN
 . ${HOME}/.acme.sh/acme.sh.env
+
+echo "Commencing test verification"
 ${HOME}/.acme.sh/acme.sh --test --issue -d ${DOMAIN} --dns dns_gcloud
+
+echo "Does everything look OK?  To continue, in uppercase with no quotes, type \"yes\"."
+read ACMERESPONSE
+if [ ${ACMERESPONSE} == "YES" ]; then
+    rm -rf ${HOME}/.acme.sh/${DOMAIN}
+    ${HOME}/.acme.sh/acme.sh --issue -d ${DOMAIN} --dns dns_gcloud
+    echo "Will now stop nginx-wrapper and install cert. To continue, in uppercase with no quotes, type \"yes\"."
+    read NGINXRESPONSE
+    if [ ${NGINXRESPONSE} == "YES" ]; then
+        docker-compose -f ${MAIN_PATH}/docker-compose.yml stop nginx-wrapper
+        rm ${MAIN_PATH}/mountpoints/nginx-wrapper/cert.key
+        rm ${MAIN_PATH}/mountpoints/nginx-wrapper/cert.crt
+        ln -s ${HOME}/.acme.sh/${DOMAIN}/${DOMAIN}.key ${MAIN_PATH}/mountpoints/nginx-wrapper/cert.key
+        ln -s ${HOME}/.acme.sh/${DOMAIN}/${DOMAIN}.cer ${MAIN_PATH}/mountpoints/nginx-wrapper/cert.crt
+        docker-compose -f ${MAIN_PATH}/docker-compose.yml up -d --no-deps --build nginx-wrapper
+    fi
+fi
+
