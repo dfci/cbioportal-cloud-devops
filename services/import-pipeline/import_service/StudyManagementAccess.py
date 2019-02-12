3d431b06925f472be54b9104bdd6f8a135f89a28
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2019-02-11 23:24:27 -0500

    access.txt fixes

--- services/import-pipeline/import_service/StudyManagementAccess.py
+++ services/import-pipeline/import_service/StudyManagementAccess.py
@@ -53,6 +53,7 @@ class AuthorizationManager(object):
     def __init__(self, local_sql: SQL_sqlite3, cbio_sql: SQL_mysql):
         self._local_sql = local_sql
         self._cbio_sql = cbio_sql
+        self.TopLevelFolderAccess = TopLevelFoldersAccess(local_sql)
         self.StudyAccess = StudyAccess(local_sql)
         self.StudyAccessAccess = StudyFileAccess(local_sql)
         self.StudyVersionAccess = StudyVersionAccess(local_sql)
@@ -64,48 +65,36 @@ class AuthorizationManager(object):
         self._run_user_sync()
 
     def _run_auth_sync(self):
-        statement = ('WITH q AS (SELECT s.id         study_id,'
-                     '                  sv.id        study_version_id,'
-                     '                  f.file_id    file_id,'
-                     '                  row_number() OVER (PARTITION BY s.id ORDER BY sv.id DESC, s.id DESC) AS _id '
-                     '           FROM study_versions sv'
-                     '                  INNER JOIN studies s ON sv.study_id = s.id'
-                     '                  INNER JOIN study_version_files f ON sv.id = f.study_version_id'
-                     "           WHERE f.file_path = 'access.txt')"
-                     'SELECT study_id, study_version_id, file_id '
-                     'FROM q '
-                     'WHERE _id = 1')
-        results = self._local_sql.exec_sql(statement)
-        for result in results:
-            study = self.StudyAccess.get_study_by_id(result[0])
-            study_version = self.StudyVersionAccess.get_study_version_by_id(result[1])
-            access_file = self.StudyAccessAccess.get_most_recent_access_file_for_study(study)
-            is_valid = None
-            authorized_emails = set() | {email for email in os.environ['ADMIN_EMAILS'].split(',')}
-            if access_file is not None:
-                for line in line_iter(access_file.get_contents()):
-                    authorized_emails.add(line.strip())
+        for top_level in self.TopLevelFolderAccess.list_all_orgs():
+            for study in top_level.get_studies():
+                study_version = self.StudyVersionAccess.get_study_version_by_id(result[1])
+                access_file = self.StudyAccessAccess.get_most_recent_access_file_for_study(study)
+                is_valid = None
+                authorized_emails = set() | {email for email in os.environ['ADMIN_EMAILS'].split(',')}
+                if access_file is not None:
+                    for line in line_iter(access_file.get_contents()):
+                        authorized_emails.add(line.strip())
 
-            meta_study_file = self.FileAccess.get_file_from_study_version_file(
-                self.FileAccess.get_meta_study_file_from_study_version(study_version))
-            meta_dict = {k: v
-                         for k, v
-                         in [(line.split(':')[0], line.split(':')[1])
-                             if ':' in line else (line, None)
-                             for line in line_iter(meta_study_file.get_contents())]}
-            cancer_study_name = meta_dict['cancer_study_identifier'].strip()
-            print("Found meta study file for study '{}' at '{}' with cancer_study_identifier as '{}'".format(
-                study.get_study_name(), meta_study_file.get_path(), cancer_study_name))
-            if is_valid is not None:
-                print("Current access.txt for study '{}' is not valid, please fix.".format(cancer_study_name))
-                break
-            print("Removing all authorizations...")
-            self.unauthorize_all_for_study(cancer_study_name)
-            for email in authorized_emails:
-                print("Authorizing email '{}' for study '{}' with cancer_study_identifier '{}".format(email,
-                                                                                                      study.get_study_name(),
-                                                                                                      cancer_study_name))
-                self.authorize_for_study(email, cancer_study_name)
+                meta_study_file = self.FileAccess.get_file_from_study_version_file(
+                    self.FileAccess.get_meta_study_file_from_study_version(study_version))
+                meta_dict = {k: v
+                             for k, v
+                             in [(line.split(':')[0], line.split(':')[1])
+                                 if ':' in line else (line, None)
+                                 for line in line_iter(meta_study_file.get_contents())]}
+                cancer_study_name = meta_dict['cancer_study_identifier'].strip()
+                print("Found meta study file for study '{}' at '{}' with cancer_study_identifier as '{}'".format(
+                    study.get_study_name(), meta_study_file.get_path(), cancer_study_name))
+                if is_valid is not None:
+                    print("Current access.txt for study '{}' is not valid, please fix.".format(cancer_study_name))
+                    break
+                print("Removing all authorizations...")
+                self.unauthorize_all_for_study(cancer_study_name)
+                for email in authorized_emails:
+                    print("Authorizing email '{}' for study '{}' with cancer_study_identifier '{}".format(email,
+                                                                                                          study.get_study_name(),
+                                                                                                          cancer_study_name))
+                    self.authorize_for_study(email, cancer_study_name)
 
     def _run_user_sync(self):
         return
