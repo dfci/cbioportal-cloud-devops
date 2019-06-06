4ef4835ce1fc78e62d419a70cb8192a565aa1b25
Eric <emarriott@feynman.dfci.harvard.edu>
2019-06-06 17:22:31 -0400

    updates

--- default_env.sh
+++ default_env.sh
@@ -3,6 +3,7 @@
 export NGINX_WRAPPER_PORT_HTTP=80
 export NGINX_WRAPPER_PORT_HTTPS=443
 export CBIOPORTAL_BRANCH="v2.0.1"
+export CBIOPORTAL_EXTERNAL_DATA="./mountpoints/cbioportal_external_data"
 export CBIOPORTAL_SEED_SQL_FILE="seed-cbioportal_hg19_v2.7.2.sql.gz"
 export CBIOPORTAL_IPV4_ADDR="172.28.10.11"
 export CBIOPORTAL_MYSQL_IPV4_ADDR="172.28.10.12"
