54078b4ec20284f92326622e2ee42d46b2baf60f
Roshni Biswas <rbiswas@dsrbiswasmacport.dfci.harvard.edu>
2019-08-14 15:50:24 -0400

    Updated branches, versions

--- default_env.sh
+++ default_env.sh
@@ -2,14 +2,14 @@
 
 export NGINX_WRAPPER_PORT_HTTP=80
 export NGINX_WRAPPER_PORT_HTTPS=443
-export CBIOPORTAL_BRANCH="v2.0.1"
+export CBIOPORTAL_BRANCH="v2.2.2"
 export CBIOPORTAL_EXTERNAL_DATA="./mountpoints/cbioportal_external_data"
-export CBIOPORTAL_SEED_SQL_FILE="seed-cbioportal_hg19_v2.7.2.sql.gz"
+export CBIOPORTAL_SEED_SQL_FILE="seed-cbioportal_hg19_v2.7.3.sql.gz"
 export CBIOPORTAL_IPV4_ADDR="172.28.10.11"
 export CBIOPORTAL_MYSQL_IPV4_ADDR="172.28.10.12"
-export ONCOKB_BRANCH="v0.3.11"
+export ONCOKB_BRANCH="v1.0.7"
 export GENOME_NEXUS_BRANCH="master"
-export GENOME_NEXUS_MONGODB_BRANCH="v0.8"
+export GENOME_NEXUS_MONGODB_BRANCH="master"
 export SESSION_SERVICE_BRANCH="master"
 export CANCERHOTSPOTS_BRANCH="master"
 export SUBNET="172.28.10.0/24"
